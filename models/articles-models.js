81c66532b7cf77c6430b782c36235404733c07f5
Claire Lancaster <hello@clairelancaster.com>
2023-02-21 11:21:06 +0000

    adding tests and functionality for task 4 - get articles

---
+++ models/articles-models.js
@@ -0,0 +1,19 @@
+const db = require("../db/connection.js");
+
+fetchArticles = () => {
+  const queryString = `
+  SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count
+  FROM articles
+  LEFT JOIN comments
+  ON comments.article_id = articles.article_id
+  GROUP BY articles.article_id
+  ORDER BY articles.created_at DESC
+  `;
+
+  return db.query(queryString).then((result) => {
+    const articles = result.rows;
+    return articles;
+  });
+};
+
+module.exports = { fetchArticles };
