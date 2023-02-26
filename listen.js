b46e98ae85d48b61108347147b4ed3ab4e995be8
Claire Lancaster <hello@clairelancaster.com>
2023-02-26 15:58:09 +0000

    prep to host

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
-const app = require('../backend-service-news/');
+const app = require("./app.js");
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
